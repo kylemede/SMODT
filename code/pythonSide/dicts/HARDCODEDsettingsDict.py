cf0b4e0ced39691cd348cb0644147cd36eeef797
kylemede <kylemede@astron.s.u-tokyo.ac.jp>
2015-02-27 23:22:24 +0900

    fixes made on lappy to get HIP10321 fit working.

--- code/pythonSide/dicts/HARDCODEDsettingsDict.py
+++ code/pythonSide/dicts/HARDCODEDsettingsDict.py
@@ -1,9 +1,9 @@
 hardcodedSettingsDict = \
 {
-    'pythonCodeDir':                '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/pythonSide',
-    'cppCodeDir':                   '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/cppSide',
-    'settings_and_InputDataDir':    '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/settings_and_InputData',
-    #'pythonCodeDir':                '/run/media/Kyle/Data1-500GB-NTFS/Todai_Work/Dropbox/workspace/SMODT/code/pythonSide',
-    #'cppCodeDir':             '/run/media/Kyle/Data1-500GB-NTFS/Todai_Work/Dropbox/workspace/SMODT/code/cppSide',
-    #'settings_and_InputDataDir':    '/run/media/Kyle/Data1-500GB-NTFS/Todai_Work/Dropbox/workspace/SMODT/SimSettings_and_InputData',
-}
+    #'pythonCodeDir':                '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/pythonSide',
+    #'cppCodeDir':                   '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/cppSide',
+    #'settings_and_InputDataDir':    '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/settings_and_InputData',
+    'pythonCodeDir':                '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/pythonSide',
+    'cppCodeDir':                   '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/cppSide',
+    'settings_and_InputDataDir':    '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/settings_and_InputData',
+}    

c6468af246e9f9b5e28125c9f488e7e0e7d7e81c
Kyle <Kyle@dhcp074.astron.s.u-tokyo.ac.jp>
2015-03-04 17:01:52 +0900

    fixed hardcoded dict vals to desktop ones.

--- code/pythonSide/dicts/HARDCODEDsettingsDict.py
+++ code/pythonSide/dicts/HARDCODEDsettingsDict.py
@@ -1,9 +1,10 @@
 hardcodedSettingsDict = \
-{
-    #'pythonCodeDir':                '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/pythonSide',
-    #'cppCodeDir':                   '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/cppSide',
-    #'settings_and_InputDataDir':    '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/settings_and_InputData',
-    'pythonCodeDir':                '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/pythonSide',
-    'cppCodeDir':                   '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/cppSide',
-    'settings_and_InputDataDir':    '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/settings_and_InputData',
+{   ## Values for Kyle's Todai Desktop
+    'pythonCodeDir':                '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/pythonSide',
+    'cppCodeDir':                   '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/code/cppSide',
+    'settings_and_InputDataDir':    '/mnt/Data1/Todai_Work/EclipseWorkspace/SMODT/settings_and_InputData',
+    ## Values for Kyle's Ultrabook
+    #'pythonCodeDir':                '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/pythonSide',
+    #'cppCodeDir':                   '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/cppSide',
+    #'settings_and_InputDataDir':    '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/settings_and_InputData',
 }    

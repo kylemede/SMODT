31427cc568bafa7e9779990742cc441a67235edc
kylemede <kylemede@gmail.com>
2015-05-15 12:29:03 +0900

    re-orginized repo to a version 1 & 2 structure.

--- smodt1/code/pythonSide/dicts/HARDCODEDsettingsDict.py
+++ smodt1/code/pythonSide/dicts/HARDCODEDsettingsDict.py
@@ -1,8 +1,8 @@
 hardcodedSettingsDict = \
 {   ## Values for Kyle's Todai Desktop
-    'pythonCodeDir':                '/run/media/kmede/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/code/pythonSide',
-    'cppCodeDir':                   '/run/media/kmede/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/code/cppSide',
-    'settings_and_InputDataDir':    '/run/media/kmede/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/settings_and_InputData',
+    'pythonCodeDir':                '/mnt/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/code/pythonSide',
+    'cppCodeDir':                   '/mnt/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/code/cppSide',
+    'settings_and_InputDataDir':    '/mnt/Data1/Todai_Work/Dropbox/EclipseWorkspaceDB/SMODT/settings_and_InputData',
     ## Values for Kyle's Ultrabook
     #'pythonCodeDir':                '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/pythonSide',
     #'cppCodeDir':                   '/run/media/kmede/SharedData/EclipseWorkspace/SMODT/code/cppSide',
